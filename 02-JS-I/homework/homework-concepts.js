6134a9b553121d9cee4e44e6e54826aede0324c7
GoncEN-94 <estebangoncebat@gmail.com>
2022-12-20 11:14:16 -0300

    concepts explain by myself

---
+++ 02-JS-I/homework/homework-concepts.js
@@ -0,0 +1,14 @@
+
+
+Homework concepts
+ 
+// Variables: una variable es un dato que no esta definido, que se puede modificar segun convenga (numeros, nombres, caracteres)
+
+// Strings: un "String" es una cadena de caracteres, que, conforman palabras, numeros, etc.
+
+// Las funciones (Argumentos, return) nos determinan donde comienza y termina (devolviendo lo especificado) un bloque de codigo
+
+// Las declaraciones "if" van a generar un comportamiento especifico del codigo dependiendo de los resultados y avances de la ejecucion.
+
+// Valores Booleanos son valores de verdad. (true, false)
+
